683d6bc16d9239c0cca2c9d462b726741e1c6ce9
Kamil Skibinski <kamil.skibinski1@gmail.com>
2026-03-09 16:54:26 +0100

    ShoppingItems Model

---
+++ Models/ShoppingItems.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingList.Models
+{
+    public class ShoppingItems
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Bought { get; set; }
+        public DateTime AddDate { get; set; } = DateTime.Now;
+    }
+}
