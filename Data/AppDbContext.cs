ed5bdfb6771390eb76e98205735e7d829298ff5b
Kamil Skibinski <kamil.skibinski1@gmail.com>
2026-03-09 17:27:14 +0100

    DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ShoppingList.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ShoppingItem> ShoppingItems { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingList.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//DB Context
+builder.Services.AddDbContext<AppDbContext>(options 
+    => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Without json =>options.UseSqlite("Data Source=C:\\Users\\KAS\\source\\repos\\ShoppingList\\ShoppingList.db")
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
