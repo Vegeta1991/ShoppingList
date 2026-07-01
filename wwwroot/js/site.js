59692ad62b2f4aa4faab1377b4145d8aff1af06c
Kamil Skibinski <kamil.skibinski1@gmail.com>
2026-07-01 14:43:13 +0200

    shopping list update

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,27 +1,43 @@
-﻿const input = document.getElementById("input");
-
+﻿
+const input = document.getElementById("input");
 const buttonAdd = document.getElementById("add-button");
-
 const buttonRemove = document.getElementById("remove-button");
-
 const list = document.getElementById("list");
 
+console.log(input, buttonAdd, buttonRemove, list);
+
 buttonAdd.addEventListener("click", function () {
+    console.log("Klik Add");
 
     const value = input.value;
+
+    if (value.trim() === "") {
+        return;
+    }
+
     const li = document.createElement("li");
     li.textContent = value;
+    li.classList.add("item");
+
+    li.addEventListener("click", function () {
+        console.log("Klik li:", li.textContent);
+        li.classList.toggle("selected");
+    });
+
     list.appendChild(li);
     input.value = "";
-
-
 });
+
 buttonRemove.addEventListener("click", function () {
-    const items = list.getElementsByTagName("li");  
-    if (items.length > 0) {
-        list.removeChild(items[items.length - 1]);
+    console.log("Klik Delete");
+
+    const selected = list.querySelector(".selected");
+    console.log("Selected:", selected);
+
+    if (selected) {
+        selected.remove();
     }
-});
+})
 
 
 
