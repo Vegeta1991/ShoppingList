7d6edb11de4025c669c1cd5d3890416164f50286
Kamil Skibinski <kamil.skibinski1@gmail.com>
2026-07-15 09:40:28 +0200

    edit button

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,43 +1,51 @@
-﻿
-const input = document.getElementById("input");
-const buttonAdd = document.getElementById("add-button");
-const buttonRemove = document.getElementById("remove-button");
+﻿const input = document.getElementById("input");
+const addButton = document.getElementById("add-button");
+const removeButton = document.getElementById("remove-button");
+const editButton = document.getElementById("edit-button");
 const list = document.getElementById("list");
 
-console.log(input, buttonAdd, buttonRemove, list);
-
-buttonAdd.addEventListener("click", function () {
-    console.log("Klik Add");
-
-    const value = input.value;
-
-    if (value.trim() === "") {
+addButton.addEventListener("click", function () {
+    if (input.value.trim() === "") { // Space check before and after txt than equal to empty string
         return;
     }
 
     const li = document.createElement("li");
-    li.textContent = value;
-    li.classList.add("item");
+    li.textContent = input.value;
+    li.classList.add("item");// add class to li element for css styling
 
     li.addEventListener("click", function () {
-        console.log("Klik li:", li.textContent);
-        li.classList.toggle("selected");
+        const selected = document.querySelector(".selected");
+
+        if (selected) {
+            selected.classList.remove("selected");
+        }
+
+        li.classList.add("selected");// add class to li element for selection styling(blue background)
     });
 
     list.appendChild(li);
     input.value = "";
 });
 
-buttonRemove.addEventListener("click", function () {
-    console.log("Klik Delete");
-
-    const selected = list.querySelector(".selected");
-    console.log("Selected:", selected);
+removeButton.addEventListener("click", function () {
+    const selected = document.querySelector(".selected");
 
     if (selected) {
         selected.remove();
     }
-})
+});
 
+editButton.addEventListener("click", function () {
+    const selected = document.querySelector(".selected");
 
+    if (!selected) {
+        alert("Wybierz element do edycji.");
+        return;
+    }
 
+    const newText = prompt("Nowa nazwa:", selected.textContent);
+
+    if (newText !== null && newText.trim() !== "") {
+        selected.textContent = newText;
+    }
+});
