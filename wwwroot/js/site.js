ced05c1225b2500873a97b43094e361dc3691c83
Kamil Skibinski <kamil.skibinski1@gmail.com>
2026-07-16 15:19:51 +0200

    icons

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -14,22 +14,22 @@ addButton.addEventListener("click", function () {
 
     const li = document.createElement("li");
     li.classList.add("item");
-
+    //checkbox
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.classList.add("checkbox");
-
+    checkbox.classList.add("fa-solid", "fa-check");
+    //product name
     const productName = document.createElement("span");
     productName.textContent = text;
     productName.classList.add("product-name");
-
+    // append checkbox and product name to li
     li.appendChild(checkbox);
     li.appendChild(productName);
-
+    // add event listeners click and change but stop propagation of click event to li
     checkbox.addEventListener("click", function (event) {
         event.stopPropagation();
     });
-
+    // toggle class bought on product name when checkbox is checked
     checkbox.addEventListener("change", function () {
         productName.classList.toggle("bought", checkbox.checked);
     });
@@ -66,8 +66,11 @@ editButton.addEventListener("click", function () {
         return;
     }
 
-    const newText = prompt("New name:", selected.textContent);
+    const productName = selected.querySelector(".product-name");// Get the product name span ( not li) element without checkbox
+
+    const newText = prompt("New name:", productName.textContent);
+
     if (newText !== null && newText.trim() !== "") {
-        selected.textContent = newText;
+        productName.textContent = newText.trim();
     }
 });
