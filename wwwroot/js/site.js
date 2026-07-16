e9e7caa22fb8943b73f0f4d77f26d8e9ae806f56
Kamil Skibinski <kamil.skibinski1@gmail.com>
2026-07-16 14:14:26 +0200

    checkbox

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,17 +1,38 @@
-﻿const input = document.getElementById("input");
+﻿
+const input = document.getElementById("input");
 const addButton = document.getElementById("add-button");
 const removeButton = document.getElementById("remove-button");
 const editButton = document.getElementById("edit-button");
 const list = document.getElementById("list");
 
 addButton.addEventListener("click", function () {
-    if (input.value.trim() === "") { // Space check before and after txt than equal to empty string
+    const text = input.value.trim();
+
+    if (text === "") {
         return;
     }
 
     const li = document.createElement("li");
-    li.textContent = input.value;
-    li.classList.add("item");// add class to li element for css styling
+    li.classList.add("item");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox");
+
+    const productName = document.createElement("span");
+    productName.textContent = text;
+    productName.classList.add("product-name");
+
+    li.appendChild(checkbox);
+    li.appendChild(productName);
+
+    checkbox.addEventListener("click", function (event) {
+        event.stopPropagation();
+    });
+
+    checkbox.addEventListener("change", function () {
+        productName.classList.toggle("bought", checkbox.checked);
+    });
 
     li.addEventListener("click", function () {
         const selected = document.querySelector(".selected");
@@ -20,11 +41,13 @@ addButton.addEventListener("click", function () {
             selected.classList.remove("selected");
         }
 
-        li.classList.add("selected");// add class to li element for selection styling(blue background)
+        li.classList.add("selected");
     });
 
     list.appendChild(li);
+
     input.value = "";
+    input.focus();
 });
 
 removeButton.addEventListener("click", function () {
@@ -39,12 +62,11 @@ editButton.addEventListener("click", function () {
     const selected = document.querySelector(".selected");
 
     if (!selected) {
-        alert("Wybierz element do edycji.");
+        alert("Chose one element");
         return;
     }
 
-    const newText = prompt("Nowa nazwa:", selected.textContent);
-
+    const newText = prompt("New name:", selected.textContent);
     if (newText !== null && newText.trim() !== "") {
         selected.textContent = newText;
     }
